cdde980bc71564a3d617c1baa589849abe8ee6eb
Cmarsman <chris_marsden@yahoo.com>
2024-05-20 09:33:02 -0500

    first exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var names = new string[] { "Rangers", "Astros", "Angels", "Mariners" };
+
+            var orderNames = names.OrderBy(name => name.Length);
+
+            foreach (var name in orderNames) 
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
